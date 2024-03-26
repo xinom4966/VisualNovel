81f3b0d2e11b212d0ce4bc4181088a0129c7b027
xinom4966 <sd.dessay@gmail.com>
2024-03-26 14:19:48 +0100

    Finished the dialogue manager

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> texts = new();
-    private int indice = 1;
+    [SerializeField] List<GameObject> sprites = new();
+    private int ind = 1;
 
 
     public void NextBtn()
     {
-        if (indice >= texts.Count || texts[indice] == null)
+        if (ind >= texts.Count && ind >= sprites.Count)
         {
             //Insert scene transition here
+            Debug.Log("Fin du dialogue");
         }
         else
         {
-            texts[indice].SetActive(true);
-            if (indice - 1 >= 0)
+            texts[ind].SetActive(true);
+            sprites[ind].SetActive(true);
+            if (ind - 1 >= 0)
             {
-                texts[indice - 1].SetActive(false);
+                texts[ind - 1].SetActive(false);
+                sprites[ind - 1].SetActive(false);
             }
         }
-        indice++;
+        ind++;
     }
 }
